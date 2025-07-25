f6cc833b5ec76e447548d5e74d91cbab857d8069
hobbybill <sungyool.park@gmail.com>
2025-07-24 21:59:43 -0700

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -2,37 +2,13 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import FilterArtistGrid from './components/FilterArtistGrid';
 import { ArtistPage } from './components/ArtistPage';
-import { PlasmicCanvasHost } from '@plasmicapp/loader-react';
-import './plasmic-init';
-
-function MapPage() {
-  return (
-    <div>
-      <h1>Explore Artists by Location & Medium</h1>
-      <FilterArtistGrid />
-    </div>
-  );
-}
-
-function HomePage() {
-  return (
-    <div className="p-2">
-      <h1>Welcome to uncommissioned</h1>
-      <a href="/map">
-        <button>Go to Map</button>
-      </a>
-    </div>
-  );
-}
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/map" element={<MapPage />} />
+        <Route path="/" element={<FilterArtistGrid />} />
         <Route path="/artist/:artistId" element={<ArtistPage />} />
-        <Route path="/plasmic-host" element={<PlasmicCanvasHost />} />
       </Routes>
     </Router>
   );
