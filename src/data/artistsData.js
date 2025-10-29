fb2600470ff6ca1fc68f71ecff960732d631e4d1
chuman-glitch <chuman@novocollective.org>
2025-10-28 19:16:29 -0600

    Update artistsData.js

--- src/data/artistsData.js
+++ src/data/artistsData.js
@@ -314,7 +314,7 @@ export const artists = [
     artistName: "Mathieu Tremblin",
     artworkTitle: "Inscripcions",
     location: "France",
-    coordinates: { lat: 48.3451, lng: 7.4341 }, // Strasbourg coordinates
+    coordinates: { lat: 48.580917, lng: 7.728222 }, // Strasbourg coordinates
     medium: ["Conceptual"],
     invisibleThread: ["You Were Here"],
     thumbnailURL: "/artist-photos/matheiu_tremblin_thumbnail.jpeg"
